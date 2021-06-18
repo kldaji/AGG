83df326f0d4106a8614a716beba926ed522babde
kldaji@naver.com <kldaji@naver.com>
2021-06-18 21:44:34 +0900

    Navigation 3.1 ~ 3.3

---
+++ Navigation/3.2/README.md
@@ -0,0 +1,30 @@
+AndroidTriviaNavigation - solution code
+=======================================
+
+1. Add navigation 
+2. Create the NavHostFragment within the activity_main layout
+3. Add fragments to the navigation graph
+4. Change the Back button's destination (pop)
+5. Add an Up Button in the app bar
+6. Add an options menu
+7. Add the navigation drawer
+
+License
+-------
+
+Copyright 2019 Google, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one or more contributor
+license agreements.  See the NOTICE file distributed with this work for
+additional information regarding copyright ownership.  The ASF licenses this
+file to you under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License.  You may obtain a copy of
+the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+License for the specific language governing permissions and limitations under
+the License.
---
+++ Navigation/3.2/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+                R.layout.fragment_title,container,false)
+
+        // playButton Click
+        binding.playButton.setOnClickListener { view : View ->
+            // navigate to gameFragment
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+
+        // set menu
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    // inflate the menu resource file
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.options_menu,menu)
+    }
+
+    // menu item click
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
+                ||super.onOptionsItemSelected(item)
+    }
+
+}
